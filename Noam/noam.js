120e741c87719538e3b0e1b68362a3fdb91cb83f
nm-ann <noam.annenberg@gmail.com>
2018-06-19 10:44:35 -0400

    commented code

--- Noam/noam.js
+++ Noam/noam.js
@@ -2,6 +2,7 @@ gender();
 industry();
 
 function gender() {
+    //data as an array of json objects, each object holds the information for its respectove 'pie slice'
     var genderData = [
         {
             "name": "women",
@@ -17,42 +18,46 @@ function gender() {
     var width = 400;
     var height = 400;
     var radius = Math.min(width, height) / 2;
-
+    //makes a canvas for the chart to appear on
     var canvas = d3.select('.gender')
         .append('svg')
         .attr('width', width)
         .attr('height', height);
-
+    //a 'g' svg element is a container used to group other svg elements
     var g = canvas.append('g')
         .attr('transform', "translate(" + width / 2 + "," + height / 2 + ')');
-
+    //generates the pie slices-their size is based on their respective amount field in the data
     var pie = d3.pie()
         .sort(null)
         .value(function(d) {
             return d.amount;
         });
+    //creates an arc which goes around the chart and in the center of it
     //setting an inner radius will make a donut chart
-    var path = d3.arc()
+    var arc = d3.arc()
         .outerRadius(radius)
         .innerRadius(0)
-        
+    //creates an arc for the labels to go around
     var label = d3.arc()
-        .outerRadius(radius - 40)
-        .innerRadius(radius - 40);
-    
-    var arc = g.selectAll('.arc')
+        .outerRadius(radius - 80)
+        .innerRadius(radius - 80);
+    //selects all arcs in the 'g' group
+    //sets the arc and pie's data to the data array and appends a 'g' to it
+    var arcs = g.selectAll('.arc')
         .data(pie(genderData))
+        //creates any 'missing' elements that the data calls for, appends a g to it, and gives it a class of arc
         .enter()
-        .append('g')
-        .attr('class', 'arc')
-
-    arc.append('path')
-        .attr('d', path)
+            .append('g')
+                .attr('class', 'arc')
+    //creates a path for each section whose color reflects the data for that section
+    arcs.append('path')
+        //d is the svg attribute for the parameters of the path, arc will make the path generate an arc 
+        .attr('d', arc)
         .attr('fill', function(d, i) {
             return d.data.color;
         })
-    
-    arc.append('text')
+    //creates text for each selection, whose positions will follow the label arc and names will reflect the data for their respective section
+    arcs.append('text')
         .attr('transform', function(d) {
             return "translate(" + label.centroid(d) + ")";
         })
@@ -64,6 +69,7 @@ function gender() {
 }
 
 function industry() {
+    //data as an array of json objects, each object holds the information for its respectove 'pie slice'
     var industryData = [
         {
             "name": "Technology",
@@ -109,42 +115,44 @@ function industry() {
     var width = 600;
     var height = 600;
     var radius = Math.min(width, height) / 2;
-
+    //makes a canvas for the chart to appear on
     var canvas = d3.select('.industry')
         .append('svg')
         .attr('width', width)
         .attr('height', height);
-
+    //a 'g' svg element is a container used to group other svg elements
     var g = canvas.append('g')
         .attr('transform', "translate(" + width / 2 + "," + height / 2 + ')');
-
+    //generates the pie slices-their size is based on their respective amount field in the data
     var pie = d3.pie()
         .sort(null)
         .value(function(d) {
             return d.amount;
         });
+    //creates an arc which goes around the chart and in the center of it
     //setting an inner radius will make a donut chart
     var path = d3.arc()
         .outerRadius(radius)
         .innerRadius(radius / 2)
         .padAngle(0.015);
-        
+    //creates an arc for the labels to go around
     var label = d3.arc()
-        .outerRadius(radius - 40)
-        .innerRadius(radius - 40);
-    
+        .outerRadius(radius - 80)
+        .innerRadius(radius - 80);
+    //selects all arcs in the 'g' group
+    //sets the arc and pie's data to the data array and appends a 'g' to it
     var arc = g.selectAll('.arc')
         .data(pie(industryData))
         .enter()
         .append('g')
         .attr('class', 'arc')
-
+    //creates a path for each section whose color reflects the data for that section
     arc.append('path')
         .attr('d', path)
         .attr('fill', function(d, i) {
             return d.data.color;
         });
-    
+    //creates text for each selection, whose positions will follow the label arc and names will reflect the data for their respective section
     arc.append('text')
         .attr('transform', function(d) {
             return "translate(" + label.centroid(d) + ")";
